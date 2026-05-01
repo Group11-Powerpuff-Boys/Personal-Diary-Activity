09850c25d24d9223562434c6882888f8caa3fbcf
Clent Lorence Mondelo <mondelo.clent1@gmail.com>
2026-05-01 12:01:19 +0800

    Added read feature (student C)

--- PDS.py
+++ PDS.py
@@ -1,16 +1,16 @@
 try:
     f = open("Diary.txt", "x")
-    f.write("04/28/2026")
+    f.write("04/28/2026\n")
     f.close()
 except FileExistsError:
-    print("File not Found")
-
+    print("File already exists")
 
 while True:
-    print("Personal Diary Application");
-    print("1. Add Diary Entry");
-    print("2. View Diary Entries");
-    print("3. Exit");
+    print("\nPersonal Diary Application")
+    print("1. Add Diary Entry")
+    print("2. View Diary Entries")
+    print("3. Exit")
+
     try:
         choice = int(input("Enter Choice: "))
 
@@ -23,13 +23,26 @@ while True:
 
             except Exception as error:
                 print("An error occurred while adding the diary:", error)
-            
+
         elif choice == 2:
-            print("view")
+            try:
+                with open("Diary.txt", "r") as f:
+                    content = f.read()
+                    if content.strip() == "":
+                        print("No diary entries found.")
+                    else:
+                        print("\n--- Diary Entries ---")
+                        print(content)
+                        print("---------------------")
+            except FileNotFoundError:
+                print("Diary file not found.")
+
         elif choice == 3:
             print("Exiting...")
             break
+
         else:
             print("Error! Try Again")
+
     except ValueError:
         print("Invalid Input")
